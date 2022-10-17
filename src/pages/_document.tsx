8fb830f41b84058d097c409edfedf507edad1eee
Hugomndez <hello@hugomendez.dev>
2022-10-17 16:19:36 -0500

    style: add overpass google font

---
+++ src/pages/_document.tsx
@@ -0,0 +1,18 @@
+import { Html, Head, Main, NextScript } from 'next/document';
+
+export default function Document() {
+  return (
+    <Html lang='en' dir='ltr'>
+      <Head>
+        <link
+          href='https://fonts.googleapis.com/css2?family=Overpass:wght@400;700&display=swap'
+          rel='stylesheet'
+        />
+      </Head>
+      <body>
+        <Main />
+        <NextScript />
+      </body>
+    </Html>
+  );
+}
